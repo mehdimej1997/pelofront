47a0ddf206206a106dee9b6b4939cf54e7398386
Mido <mido@pop-os.localdomain>
2022-03-26 12:45:57 +0100

    finall version

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarComputer from "./NavbarComputer";
 import NavbarMobile from "./NavbarMobile";
 
+function debounce(fn, ms) {
+  let timer;
+  return (_) => {
+    clearTimeout(timer);
+    timer = setTimeout((_) => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
+
 function Navbar() {
-  return (
-    <>
-      {/* <NavbarComputer /> */}
-      <NavbarMobile />
-    </>
-  );
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        width: window.innerWidth,
+      });
+    }, 100);
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return (_) => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  }, []);
+  console.log(dimensions);
+  return <>{dimensions.width > 900 ? <NavbarComputer /> : <NavbarMobile />}</>;
 }
 
 export default Navbar;
